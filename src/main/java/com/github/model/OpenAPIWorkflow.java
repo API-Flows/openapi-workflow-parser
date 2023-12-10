6c50ea3aeec9ecca734830867935b3f2981ca831
gcatanese <gcatanese@yahoo.com>
2023-12-10 14:04:43 +0100

    Add Maven project

---
+++ src/main/java/com/github/model/OpenAPIWorkflow.java
@@ -0,0 +1,4 @@
+package com.github.model;
+
+public class OpenAPIWorkflow {
+}
---
+++ src/main/java/com/github/parser/OpenAPIWorkflowParser.java
@@ -0,0 +1,12 @@
+package com.github.parser;
+
+import com.github.model.OpenAPIWorkflow;
+
+public class OpenAPIWorkflowParser {
+
+    public OpenAPIWorkflow parser(String location) {
+        OpenAPIWorkflow openAPIWorkflow = new OpenAPIWorkflow();
+
+        return openAPIWorkflow;
+    }
+}
